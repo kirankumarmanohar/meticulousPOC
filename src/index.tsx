70af3dadb2847d30b555766ceb5ab2ce216e0765
Kiran <kiran@ideas2it.com>
2024-02-16 16:46:07 +0530

    index.js file changes

--- src/index.tsx
+++ src/index.tsx
@@ -6,22 +6,45 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 
+import { tryLoadAndStartRecorder } from '@alwaysmeticulous/recorder-loader'
 const queryClient = new QueryClient()
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <div className='App'>
-        <RouterProvider router={router} />
-      </div>
-    </QueryClientProvider>
-  </React.StrictMode>
-);
+async function startApp() {
+  // Record all sessions on localhost, staging stacks and preview URLs
+  
+    // Start the Meticulous recorder before you initialise your app.
+    // Note: all errors are caught and logged, so no need to surround with try/catch
+    await tryLoadAndStartRecorder({
+      projectId: 'VtmwqwwjcneICsCY0NqdGDlHV0XzU6JTzvq0DMyz',
+      isProduction: false,
+    });
+  
+
+  // // Initalise app after the Meticulous recorder is ready, e.g.
+  // ReactDOM.render(component, document.getElementById('root'));
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+  );
+  
+  root.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <div className='App'>
+          <RouterProvider router={router} />
+        </div>
+      </QueryClientProvider>
+    </React.StrictMode>
+  );
+  
+}
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+startApp();
+
+
